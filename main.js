c3d6d17df01bf35683e1738e7e60b0ff4de09795
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-03-17 21:13:00 +0700

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+let arrayofjokes = [
+    "Did you try reading the docs? They are useful.",
+    "Powered by advanced cat technologies",
+    "Clearly Cyno has not fixed the installer yet",
+    "I'm not sure if this is a bug or a feature",
+    "Coming soon in PlayCover Hourly",
+]
+
+function openLink1() {
+    // Open rickroll link
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+function openLink2() {
+    // Open rickroll but different link
+    window.open("https://www.youtube.com/watch?v=iik25wqIuFo");
+}
+
+function onload() {
+    // replace the h2 element with id date with the current date
+    document.getElementById("date").innerHTML = " " + new Date().toDateString() + " ";
+
+    // update the p element with id joke with a random joke from the array
+    document.getElementById("joke").innerHTML = " " + arrayofjokes[Math.floor(Math.random() * arrayofjokes.length)] + " ";
+}
